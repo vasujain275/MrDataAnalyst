ef3a38727922645d360c4d553e81ed1b9ee56297
Vasu Jain <69643310+vasujain2@users.noreply.github.com>
2022-10-16 00:51:48 +0530

    Interface Completed

--- main.py
+++ main.py
@@ -37,36 +37,43 @@ else:
 print("-------------------------------")
 print(f'Here is your Data Frame\n{df}')
 print("-------------------------------")
-
-print("What you want to do with your Data?")
-print('''
-1. Visualize
-2. Aggregate
-3. Sort
-''')
-datan = int(input("> "))
-
-if datan==1:
-    print("Which type of Data Visualization you want?")
+opn ="y"
+while opn=='y':
+    print("What you want to do with your Data?")
     print('''
-    1. Single Line Plot
-    2. Multiple Line Plot
-    3. Multiple View Line Plot
-    4. Bar Plot
-    5. Histogram
+    1. Visualize
+    2. Aggregate
+    3. Sort
     ''')
-    visn = int(input("> "))
-elif datan==2:
-    print("Which type of Data Aggregation you want?")
-    print('''
-    1. Maximum Value
-    2. Minimum Value
-    3. Sum of Values
-    4. Count of Values
-    5. Mean of Values
-    6. Median of Values
-    7. Mode of Values
-    8. Quantile of Values
-    9. Grouping & Transforming Data
-    ''')
-    aggrn = int(input("> "))
+    datan = int(input("> "))
+
+    if datan==1:
+        print("Which type of Data Visualization you want?")
+        print('''
+        1. Single Line Plot
+        2. Multiple Line Plot
+        3. Multiple View Line Plot
+        4. Bar Plot
+        5. Histogram
+        ''')
+        visn = int(input("> "))
+    elif datan==2:
+        print("Which type of Data Aggregation you want?")
+        print('''
+        1. Maximum Value
+        2. Minimum Value
+        3. Sum of Values
+        4. Count of Values
+        5. Mean of Values
+        6. Median of Values
+        7. Mode of Values
+        8. Quantile of Values
+        9. Grouping & Transforming Data
+        ''')
+        aggrn = int(input("> "))
+    elif datan==3:
+        print("Here is your Sorted Data")
+    opn = input("Do you want to continue(y/n): ")
+
+print("\nThank you for using Data Anaylist Tool\n")
+print("Made by - \nJatin Gupta (XII D)\nVasu Jain (XII B)")
