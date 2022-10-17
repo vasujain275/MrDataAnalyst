d955a899f5f24628d6a580e21e5c343f91019da2
Vasu Jain <69643310+vasujain2@users.noreply.github.com>
2022-10-17 23:06:23 +0530

    Added Max aggr function

---
+++ aggr.py
@@ -0,0 +1,30 @@
+import pandas as pd
+def amax(df):
+    print("Which type of maximum value you want?(1/2/3)")
+    print('''
+    1. Maximum value of all columns in Dataframe
+    2. Maximum value of all rows in Dataframe
+    3. Maximum value of specifed column in Dataframe
+    ''')
+    coln = int(input("> "))
+    numn = input("Do you want only numeric values?(y/n): ")
+    if numn=='y':
+        numo = True
+    elif numn=='n':
+        numo=False
+
+    if coln==1:
+        print(df.max())
+    elif coln==2:
+        print(df.max(axis=1))
+    elif coln==3:
+        col = input("Enter the Exact Name of Column you want to use: ")
+        print(df[col].max)
+    else:
+        print("Choose between 1/2/3")
+
+d = {"Name":['Vasu','Jatin'],
+     'Marks':[100,90]
+    }
+df = pd.DataFrame(d)
+print(amax(df))
