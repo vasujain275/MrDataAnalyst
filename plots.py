f99b1d2079add1c67c26c275c12da4d6463b5290
JatinGupta14 <115945095+JatinGupta14@users.noreply.github.com>
2022-10-18 23:43:14 +0530

    Single Line Complete

--- plots.py
+++ plots.py
@@ -1,13 +1,22 @@
+from turtle import color, width
 import pandas as pd
 import matplotlib.pyplot as plt
 
-def SingelLine(d):
-    plt.plot(d)
+def SingelLine(df):
+    namex = input("What do you want on X-axis: ")
+    x = df[namex]
+    namey = input("What do you want on Y-axis: ")
+    y = df[namey]
+    plt.plot(x,y,color='Red')
+    plt.xlabel(input("Enter X-label: "))
+    plt.ylabel(input("Enter Y-label: "))
+    plt.show()
 
-d = {"Name":['Vasu','Jatin'],
-     'Marks':[100,90]
+
+
+d = {"Name":['Vasu','Jatin','Rachit','Daksh'],
+     'Marks':[100,90,45,50]
     }
 df = pd.DataFrame(d)
 print(SingelLine(df))
-
 
